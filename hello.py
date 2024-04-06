2b9b6ec7e0ddabb973af489871f85d1df045e2d3
annieseinna <107967636+annieseinna@users.noreply.github.com>
2024-04-05 22:44:00 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
-print("Hello, my name is Annie")
+print("Hello, my name is Arik")
+
+#edited by Maddie
+print("Hello, my name is Maddie")
+
+#edited by Esther
+print("Hello, my name is Esther")
+
+#edited by Zach
+print("Hello, my name is Zach")
 
 #edited by Annie
 print("Hello, my name is Annie")
