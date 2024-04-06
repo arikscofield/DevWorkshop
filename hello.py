a943242d48fdffa91ab014e654916b4d3ee81655
Annie Tran <anniettran@hackberry.rlogin>
2024-04-05 22:39:31 -0400

    edited print statement

--- hello.py
+++ hello.py
@@ -1,13 +1,1 @@
-print("Hello, my name is Arik")
-
-#edited by Maddie
-print("Hello, my name is Maddie")
-
-#edited by Esther
-print("Hello, my name is Esther")
-
-#edited by Zachary
-print("Hello, my name is Zachary")
-
-#edited by Annie
 print("Hello, my name is Annie")
