5980ef054185257ebea81283e93a65179dcd97a1
Zachary Lahlou <lahlouzn@sumac.rlogin>
2024-04-05 21:33:53 -0400

    changes

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello, my name is Arik")
+
+#edited by Maddie
+print("Hello, my name is Maddie")
+
+#edited by Esther
+print("Hello, my name is Esther")
+
+#edited by Zack
+print("Hello, my name is Zack")
