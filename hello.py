c251c516ed9f931e4233d6a8a43d2ddffa107b3b
Zachary Lahlou <lahlouzn@sumac.rlogin>
2024-04-05 21:27:10 -0400

    Zachary Lahlou (lahlouzn) added files

--- hello.py
+++ hello.py
@@ -1,7 +1,1 @@
-print("Hello, my name is Arik")
-
-#edited by Maddie
-print("Hello, my name is Maddie")
-
-#edited by Esther
-print("Hello, my name is Esther")
+print("Hello, my name is Zachary Lahlou")
